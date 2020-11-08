f2a77a724755a046e7f3e18091e873b81cdd3cbe
Josiah Travis <josiahtravis1@gmail.com>
2020-11-07 22:16:25 -0800

    added equality and inequality operators for product type

--- StoreApp/StoreApp.Library/Product.cs
+++ StoreApp/StoreApp.Library/Product.cs
@@ -22,6 +22,19 @@ namespace StoreApp.Library
             Name = name;
             Price = price;
         }
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Product);
+        }
+        public static bool operator ==(Product c1, Product c2)
+        {
+            if (c1.Equals(c2)) return true;
+            return false;
+        }
+        public static bool operator !=(Product c1, Product c2)
+        {
+            return (!(c1 == c2));
+        }
         public void AddDiscount(double d)
         {
             _discount = d;
